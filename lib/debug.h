94cc67432e299110781d1342a48ba6871b8be2a4
Damien Hedde <damien.hedde@hikob.com>
2014-01-21 17:30:46 +0100

    add NO_DEBUG_COLOR macro to disable color in log printfs

--- lib/debug.h
+++ lib/debug.h
@@ -52,40 +52,62 @@
 #define LOG_LEVEL_DISABLED 4
 
 #ifndef NO_DEBUG_HEADER
+#ifndef NO_DEBUG_COLOR
 #define DEBUG_HEADER()             printf("\x1b[32m[in %s() DEBUG] ", __FUNCTION__)
 #define INFO_HEADER()              printf("\x1b[33m[in %s() INFO] ", __FUNCTION__)
 #define WARNING_HEADER()           printf("\x1b[35m[in %s() WARNING] ", __FUNCTION__)
 #define ERROR_HEADER()             printf("\x1b[31m[in %s() ERROR] ", __FUNCTION__)
 #define NOT_IMPLEMENTED_HEADER()   printf("\x1b[36m[in %s() NOT IMPLEMENTED] ", __FUNCTION__)
 #else
+#define DEBUG_HEADER()             printf("[in %s() DEBUG] ", __FUNCTION__)
+#define INFO_HEADER()              printf("[in %s() INFO] ", __FUNCTION__)
+#define WARNING_HEADER()           printf("[in %s() WARNING] ", __FUNCTION__)
+#define ERROR_HEADER()             printf("[in %s() ERROR] ", __FUNCTION__)
+#define NOT_IMPLEMENTED_HEADER()   printf("[in %s() NOT IMPLEMENTED] ", __FUNCTION__)
+#endif
+#else
+#ifndef NO_DEBUG_COLOR
 #define DEBUG_HEADER()             printf("\x1b[32m")
 #define INFO_HEADER()              printf("\x1b[33m")
 #define WARNING_HEADER()           printf("\x1b[35m")
 #define ERROR_HEADER()             printf("\x1b[31m")
 #define NOT_IMPLEMENTED_HEADER()   printf("\x1b[36m")
+#else
+#define DEBUG_HEADER()
+#define INFO_HEADER()
+#define WARNING_HEADER()
+#define ERROR_HEADER()
+#define NOT_IMPLEMENTED_HEADER()
+#endif
+#endif
+
+#ifndef NO_DEBUG_COLOR
+#define DEBUG_ENDL()               printf("\x1b[0m\n")
+#else
+#define DEBUG_ENDL()               printf("\n")
 #endif
 
 #ifndef LOG_LEVEL
 #define LOG_LEVEL LOG_LEVEL_DEBUG
 #endif // LOG_LEVEL
 #if (LOG_LEVEL <= LOG_LEVEL_DEBUG)
-#define log_debug(...) do {DEBUG_HEADER(); printf(__VA_ARGS__);printf("\x1b[0m\n");}while(0)
+#define log_debug(...) do {DEBUG_HEADER(); printf(__VA_ARGS__);DEBUG_ENDL();}while(0)
 #else // (LOG_LEVEL <= LOG_LEVEL_DEBUG)
 #define log_debug(...)
 #endif // (LOG_LEVEL <= LOG_LEVEL_DEBUG)
 #if (LOG_LEVEL <= LOG_LEVEL_INFO)
-#define log_info(...) do {INFO_HEADER(); printf(__VA_ARGS__);printf("\x1b[0m\n");}while(0)
+#define log_info(...) do {INFO_HEADER(); printf(__VA_ARGS__);DEBUG_ENDL();}while(0)
 #else // (LOG_LEVEL <= LOG_LEVEL_INFO)
 #define log_info(...)
 #endif // (LOG_LEVEL <= LOG_LEVEL_INFO)
 #if (LOG_LEVEL <= LOG_LEVEL_WARNING)
-#define log_warning(...) do {WARNING_HEADER(); printf(__VA_ARGS__);printf("\x1b[0m\n");}while(0)
+#define log_warning(...) do {WARNING_HEADER(); printf(__VA_ARGS__);DEBUG_ENDL();}while(0)
 #else // (LOG_LEVEL <= LOG_LEVEL_INFO)
 #define log_warning(...)
 #endif // (LOG_LEVEL <= LOG_LEVEL_INFO)
 #if (LOG_LEVEL <= LOG_LEVEL_ERROR)
-#define log_error(...) do {ERROR_HEADER(); printf(__VA_ARGS__);printf("\x1b[0m\n");}while(0)
-#define log_not_implemented(...) do {NOT_IMPLEMENTED_HEADER(); printf(__VA_ARGS__);printf("\x1b[0m\n");}while(0)
+#define log_error(...) do {ERROR_HEADER(); printf(__VA_ARGS__);DEBUG_ENDL();}while(0)
+#define log_not_implemented(...) do {NOT_IMPLEMENTED_HEADER(); printf(__VA_ARGS__);DEBUG_ENDL();}while(0)
 #else // (LOG_LEVEL <= LOG_LEVEL_ERROR)
 #define log_error(...)
 #define log_not_implemented(...)
